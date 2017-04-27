58a1e5dc702361a45c79a27499137462d00c3a82
Paula Ponce <paula.poncez@usach.cl>
2017-04-27 11:22:43 -0300

    imprimir nueva tarea en main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
+//función para mostras las 10 tareas de JSON. JSON está en data.js
 function mostrarTarea(){
 	var tareas10 = res.forEach(function(element){
-		document.write('<ul><li>' + element.title + '</li></ul>');
+		document.write('<ul><li>' + element.title + '</li></ul>'); //tares en una lista
 		//document.getElementById('cont-tarea').innerHTML = element.title + "<br>";
 	});
 }
 mostrarTarea();
+
+var tareas = [];
+var nuevaTarea = document.getElementById('cont-nuevaTarea');
+
+//constructor para generar una nueva tarea
+function NuevaTarea(userId, id, title, completed){
+	this.userId = userId;
+	this.id = id;
+	this.title = title;
+	this.completed = completed;
+};
+
+//método para imprimir la nueva tarea en html 
+NuevaTarea.prototype.toHTML = function(){
+	var html = " ";
+	html += this.id + "<br>";
+	html += this.title + "<br>";
+	html += this.completed + "<br>";
+	return html;
+}
+
