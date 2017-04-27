13c61a5956e91dbbcf6fcb034d16eb32fdd7fa2f
Paula Ponce <paula.poncez@usach.cl>
2017-04-27 11:16:37 -0300

    constructor para generar nueva tarea en main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 function mostrarTarea(){
-
 	var tareas10 = res.forEach(function(element){
 		document.write('<ul><li>' + element.title + '</li></ul>');
 		//document.getElementById('cont-tarea').innerHTML = element.title + "<br>";
 	});
-
 }
 mostrarTarea();
